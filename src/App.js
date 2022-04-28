d6fb50effc4a5eac81bc89af28a73360fd4223bf
dy96 <daniel.yaacobi@mail.huji.ac.il>
2022-04-28 15:27:06 +0300

    updated files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Form} from "./components";
+import {Navigation} from "./components";
+
+function App() {
+    return (
+        // <div className="navigation-component">
+        //     <
+        // </div>
+        <div className="form-component">
+            <Form/>
+        </div>
+    );
+}
+
+export default App;
