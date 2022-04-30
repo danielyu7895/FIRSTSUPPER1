64b183d0fbd198602d627678068d39f9f53b48f4
noarozmann <noa.rozmann@mail.huji.ac.il>
2022-04-30 17:07:22 +0300

    something

---
+++ src/components/Form2.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import "./formStyle2.css"
+
+export default function Form2() {
+
+    return (
+        <div className="form2">
+            <form>
+                <h1 className='headline'>Choose what you like</h1>
+
+
+            </form>
+        </div>
+    );
+}
+
