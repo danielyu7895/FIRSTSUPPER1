bab8ad3ea430cf9b784214306ab75668cbfe4528
noarozmann <noa.rozmann@mail.huji.ac.il>
2022-05-02 20:28:21 +0300

    something

--- src/Components/InterestButton.js
+++ src/Components/InterestButton.js
@@ -1,32 +1,34 @@
 import React, { useState } from 'react'
 import "../Styles/InterestButton.css"
 
-
-const onMouseOver = event => {
-    const el = event.target;
-    let colorhex = [
-        "#3498DB"
-    ];
-    el.style.backgroundColor = colorhex
-};
-
-const onMouseOut = event => {
-    const el = event.target;
-    let white = "#ffffff";
-    el.style.backgroundColor = white;
-};
+//
+// const onMouseOver = event => {
+//     const el = event.target;
+//     let colorhex = [
+//         "#3498DB"
+//     ];
+//     el.style.backgroundColor = colorhex
+// };
+//
+// const onMouseOut = event => {
+//     const el = event.target;
+//     let white = "#ffffff";
+//     el.style.backgroundColor = white;
+// };
 
 function InterestButton({ children }) {
     const [backgroundColor, setBackgroundColor] = useState(false)
     return (
-        <button onClick={() => setBackgroundColor(!backgroundColor)} className={`${backgroundColor && 'active'} 
-        {'${backgroundColor && 'hover'} profile-interests-tag`}
-                onMouseEnter={event => onMouseOver(event)}
-                onMouseOut={event => onMouseOut(event)}>
+        <button onClick={() => setBackgroundColor(!backgroundColor)} className={`${backgroundColor && 'active'} profile-interests-tag`}>
+
             {children}
 
         </button>
     )
 }
+
+
+
+
 
 export default InterestButton
