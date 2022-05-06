91ce218b3fe4c6f4da07035f823405d54f8b5099
noarozmann <noa.rozmann@mail.huji.ac.il>
2022-05-06 19:33:40 +0300

    something

--- src/Pages/EventPage.js
+++ src/Pages/EventPage.js
@@ -58,12 +58,11 @@ export default function EventPage() {
                                     </div>
                                     <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                                         <div className="py-6 px-3 mt-32 sm:mt-0">
-                      {/*                       <button*/}
-                      {/*  className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"*/}
-                      {/*  type="button"*/}
-                      {/*>*/}
-                      {/*  Connect*/}
-                      {/*</button> */}
+                                             <button>
+                                                 <Link to="/JoinEvent" className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"> Join</Link>
+
+                      </button>
+
                                         </div>
                                     </div>
                                     <div className="w-full lg:w-4/12 px-4 lg:order-1">
@@ -150,7 +149,6 @@ export default function EventPage() {
                                                 <Tag name="Singing"/><Tag name="Football"/><Tag name="Dancing"/><Tag name="Reading"/><Tag name="Wine Testing"/>
 
                                             </div>
-                                            <Link to="/JoinEvent" className="font-bold text-blueGray-700 mt-8">more info</Link>
                                         </div>
                                     </div>
                                 </div>
