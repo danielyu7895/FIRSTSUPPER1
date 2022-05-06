bb7760266bba998c4369d81b4bfca6768d1b529e
danielyu7895 <104500759+danielyu7895@users.noreply.github.com>
2022-05-06 14:52:20 +0300

    Update EventCard.js

--- src/newPages/EventCard.js
+++ src/newPages/EventCard.js
@@ -1,3 +1,7 @@
+// not finished yet!!
+// need to be in components folder, in new folder coled Cards
+// used in EventPage.js
+
 import React from 'react'
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
