d96f722dc858add7331e5835c69c9d914e299400
danielyu7895 <104500759+danielyu7895@users.noreply.github.com>
2022-05-07 14:54:46 +0300

    Update EventsHeader.js

--- src/Components/EventsHeader.js
+++ src/Components/EventsHeader.js
@@ -1,5 +1,3 @@
-// not finished yet!!
-// need to be in components folder
 // used in EventPage.js
 
 import React from 'react'
@@ -14,47 +12,29 @@ function EventsHeader() {
                 className="absolute top-0 w-full h-full bg-center bg-cover"
                 style={{
                 backgroundImage:
-                    "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+                    "url('https://c.stocksy.com/a/5CG900/z9/2207205.jpg')",
                 }}
             >
                 <span
                 id="blackOverlay"
-                className="w-full h-full absolute opacity-75 bg-black"
+                className="w-full h-full absolute opacity-50 bg-black"
                 ></span>
             </div>
             <div className="container relative mx-auto">
                 <div className="items-center flex flex-wrap">
-                <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                    <div className="pr-12">
-                    <h1 className="text-white font-semibold text-5xl">
-                        Let's Shihi together
-                    </h1>
-                    <h2 className="mt-4 text-lg text-blueGray-200">
-                        Here is Shihi dinners for the next 2 weeks
-                    </h2>
+                    <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                        <div className="pr-12">
+                            <h1 className=" my-4 text-white font-bold text-5xl">
+                                Let's Shihi together
+                            </h1>
+                            <h2 className="my-4 text-xl text-blueGray-200">
+                             Here is Shishi dinners for you
+                            </h2>
+                        </div>
                     </div>
                 </div>
-                </div>
             </div>
-            <div
-                className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-                style={{ transform: "translateZ(0)" }}
-            >
-                <svg
-                className="absolute bottom-0 overflow-hidden"
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-                >
-                <polygon
-                    className="text-blueGray-200 fill-current"
-                    points="2560 0 2560 100 0 100"
-                ></polygon>
-                </svg>
-            </div>
+            
         </div>
     </>
   )
