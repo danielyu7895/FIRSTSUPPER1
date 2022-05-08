23e2a80256316c17557b202c98da5b737c81ab16
noarozmann <noa.rozmann@mail.huji.ac.il>
2022-05-08 15:34:04 +0300

    something

--- src/newPages/JoinEvent.js
+++ src/newPages/JoinEvent.js
@@ -233,7 +233,7 @@ export default function JoinEvent() {
                                             {/*</button>*/}
                                             {/*<Modal open={this.state.setShowModal}></Modal>*/}
                                             <button className="bg-purple-dark text-white active:bg-purple-dark font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                                            //         onClick={() => this.setState({setShowModal: true})}
+                                            {/*//         onClick={() => this.setState({setShowModal: true})}*/}
                                                 Join
                                             </button>
                                         </div>
